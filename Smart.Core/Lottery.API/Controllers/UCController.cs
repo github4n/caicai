0189e0ebf4bedd4705ac5d5b1d1b8ac00a50d0a3
刘杭州 <348846313@qq.com>
2018-12-28 15:34:59 +0800

    非高频彩逻辑实现

--- Smart.Core/Lottery.API/Controllers/UCController.cs
+++ Smart.Core/Lottery.API/Controllers/UCController.cs
@@ -37,6 +37,10 @@ namespace Lottery.API.Controllers
                 foreach (XmlElement CurrenNode in nodes)
                 {
                     var Common = commonConfig.Find((x) => x.key == CurrenNode.ChildNodes[0].InnerText);
+                    if (Common == null)
+                    {
+                        continue;
+                    }
                     var item = CurrenNode.ChildNodes[2];
                     foreach (XmlElement Subitem in item.ChildNodes)
                     {
@@ -70,7 +74,6 @@ namespace Lottery.API.Controllers
                 throw new Exception(ex.Message);
             }
         }
-
         /// <summary>
         /// 高频彩：http://lottery.jdddata.com/uc/highfreq
         /// </summary>
@@ -78,9 +81,58 @@ namespace Lottery.API.Controllers
         [HttpGet]
         public async Task<string> highfreq()
         {
-            var xmlDoc = LoadXmlDocument("http://lottery.jdddata.com/uc/highfreq");
-            var commonConfig = ConfigFileHelper.Get<List<CommonModel>>("HeightLottery");
-            return await Task.Run(() => "1111");
+            try
+            {
+                var xmlDoc = LoadXmlDocument("http://lottery.jdddata.com/uc/highfreq");
+                var commonConfig = ConfigFileHelper.Get<List<HeightLottery>>("HeightLottery");
+                var nodes = xmlDoc.DocumentElement.ChildNodes;
+                foreach (XmlElement element in nodes)
+                {
+                    var Common = commonConfig.Find((x) => x.key == element.ChildNodes[0].InnerText);
+                    if (Common == null)
+                    {
+                        continue;
+                    }
+                    foreach (XmlElement Sub_element in element.LastChild)
+                    {
+                        if (Sub_element.Name == "title_url")
+                        {
+                            Sub_element.InnerText=Common.title_url;
+                        }
+                        if (Sub_element.Name == "bet")
+                        {
+                            var remark = Common.foot_group.Where(x => x.remark == Sub_element.FirstChild.InnerText).FirstOrDefault();
+                            if (remark == null)
+                            {
+                                continue;
+                            }
+                              ((XmlElement)Sub_element.LastChild).InnerText = remark.url;
+                        }
+                        if (Sub_element.Name == "foot_group")
+                        {
+                            foreach (XmlElement Sub_foot_group in Sub_element.ChildNodes)
+                            {
+                                    if (Sub_foot_group.Attributes["name"].InnerText == "开奖详情")
+                                    {
+                                    Sub_foot_group.SetAttribute("url", Common.foot_group.Where(x => x.remark == Sub_foot_group.Attributes["name"].InnerText).FirstOrDefault().url);
+                                    }
+                                    else if (Sub_foot_group.Attributes["name"].InnerText == "玩法说明")
+                                    {
+                                    Sub_foot_group.SetAttribute("url", Common.foot_group.Where(x => x.remark == Sub_foot_group.Attributes["name"].InnerText).FirstOrDefault().url);
+                                    }
+
+                            }
+                        }
+                    }
+                }
+                RedisManager.DB_Other.Set("UC_highfreq", xmlDoc.InnerXml);
+                var b = RedisManager.DB_Other.Get("UC_highfreq");
+                return await Task.Run(() => xmlDoc.InnerXml);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
         /// <summary>
@@ -90,7 +142,68 @@ namespace Lottery.API.Controllers
         [HttpGet]
         public async Task<string> nonhighfreq()
         {
-            return await Task.Run(() => "1111");
+            try
+            {
+                var xmlDoc = LoadXmlDocument("http://lottery.jdddata.com/uc/nonhighfreq");
+                var commonConfig = ConfigFileHelper.Get<List<NoHeightLottery>>("NoHeightLottery");
+                var nodes = xmlDoc.DocumentElement.ChildNodes;
+                foreach (XmlElement element in nodes)
+                {
+                    var Common = commonConfig.Where(x => x.key == element.FirstChild.InnerText).FirstOrDefault();
+                    if (Common == null)
+                    {
+                        continue;
+                    }
+                    var Issue = ((XmlElement)element.GetElementsByTagName("qihao")[0]).InnerText;
+                    if (string.IsNullOrEmpty(Issue))
+                    {
+                        continue;
+                    }
+                    var titelUrl = Common.title_url + Issue;
+                    var morelink = Common.morelink + Issue;
+                    foreach (XmlElement Sub_element in element.LastChild)
+                    {
+                        if (Sub_element.Name == "title_url")
+                        {
+                            Sub_element.InnerText= titelUrl;
+                        }
+                        if (Sub_element.Name == "bet")
+                        {
+                            var remark = Common.foot_group.Where(x => x.remark == Sub_element.FirstChild.InnerText).FirstOrDefault();
+                            if (remark == null)
+                            {
+                                continue;
+                            }
+                            ((XmlElement)Sub_element.LastChild).InnerText= remark.url;
+                        }
+                        if (Sub_element.Name == "foot_group")
+                        {
+                            foreach (XmlElement Sub_foot_group in Sub_element.ChildNodes)
+                            {
+                                if (Sub_foot_group.Attributes["name"].InnerText == "开奖详情")
+                                {
+                                    Sub_foot_group.SetAttribute("url", titelUrl);
+                                }
+                                else if (Sub_foot_group.Attributes["name"].InnerText == "玩法说明")
+                                {
+                                    Sub_foot_group.SetAttribute("url", Common.foot_group.Where(x => x.remark == Sub_foot_group.Attributes["name"].InnerText).FirstOrDefault().url);
+                                }
+                            }
+                        }
+                        if (Sub_element.Name == "morelink")
+                        {
+                            Sub_element.SetAttribute("url", morelink);
+                        }
+                    }
+                }
+                RedisManager.DB_Other.Set("UC_nonhighfreq", xmlDoc.InnerXml);
+                var b = RedisManager.DB_Other.Get("UC_nonhighfreq");
+                return await Task.Run(() => xmlDoc.InnerXml);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
         /// <summary>
@@ -105,7 +218,7 @@ namespace Lottery.API.Controllers
         protected XmlDocument LoadXmlDocument(string URI)
         {
             XmlDocument xmlDoc = new XmlDocument();
-            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create("http://lottery.jdddata.com/uc/common");
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(URI);
             HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse();
             using (Stream streamReceive = webResponse.GetResponseStream())
             {
--- Smart.Core/Lottery.Modes/Model/NoHeightLottery.cs
+++ Smart.Core/Lottery.Modes/Model/NoHeightLottery.cs
@@ -4,16 +4,12 @@ using System.Text;
 
 namespace Lottery.Modes.Model
 {
-   public class HeightLottery
+   public class NoHeightLottery
     {
         public string key { get; set; }
         public string title { get; set; }
         public string title_url { get; set; }
+        public string morelink { get; set; }
         public List<foot_group> foot_group { get; set; }
-    }
-    public class foot_group
-    {
-        public string url { get; set; }
-        public string remark { get; set; }
     }
 }
