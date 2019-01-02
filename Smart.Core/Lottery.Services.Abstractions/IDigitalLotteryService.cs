488e2e7ad0995ef56e7f2bb273ea4c8fc4b2021b
刘杭州 <348846313@qq.com>
2019-01-02 11:07:32 +0800

    足球单场胜负过关

--- Smart.Core/Lottery.Services.Abstractions/IDigitalLotteryService.cs
+++ Smart.Core/Lottery.Services.Abstractions/IDigitalLotteryService.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Text;
 using EntityModel.Model;
+using Lottery.Modes.Entity;
 
 namespace Lottery.Services.Abstractions
 {
    public interface IDigitalLotteryService
     {
         void Addnormal_lotterydetail(List<fc3D> ModelList);
+        void AddZqdc_Sfgg(List<zqdc_sfgg_result> zqdc_Sfggs);
+        string Getnormal_lotteryIssue();
+        string GetZqdc_Sfgg();
     }
 }
