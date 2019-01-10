6033ad8d08ed3bcd6cc08ba1ad6c5f60d80c7ea4
刘杭州 <348846313@qq.com>
2019-01-10 10:11:55 +0800

    放回彩种字典

--- Smart.Core/Lottery.Modes/Model/KaiJiangWangDic.cs
+++ Smart.Core/Lottery.Modes/Model/KaiJiangWangDic.cs
@@ -17,35 +17,35 @@ namespace Lottery.Modes.Model
         static KaiJiangWangDic()
         {
             //接口路径字典
-            //GaoPindic.Add("gdklsf", "kuai10/10301");
-            //GaoPindic.Add("gdsyxw", "selected5/10201");
-            //GaoPindic.Add("shhsyxw", "selected5/10203");
-            //GaoPindic.Add("ahsyxw", "selected5/10204");
-            //GaoPindic.Add("dlc", "selected5/10205");
-            //GaoPindic.Add("jlsyxw", "selected5/10206");
-            //GaoPindic.Add("gxsyxw", "selected5/10207");
-            //GaoPindic.Add("hbsyxw", "selected5/10208");
-            //GaoPindic.Add("lnsyxw", "selected5/10209");
-            //GaoPindic.Add("jssyxw", "selected5/10210");
-            //GaoPindic.Add("zjsyxw", "selected5/10211");
+            GaoPindic.Add("gdklsf", "kuai10/10301");
+            GaoPindic.Add("gdsyxw", "selected5/10201");
+            GaoPindic.Add("shhsyxw", "selected5/10203");
+            GaoPindic.Add("ahsyxw", "selected5/10204");
+            GaoPindic.Add("dlc", "selected5/10205");
+            GaoPindic.Add("jlsyxw", "selected5/10206");
+            GaoPindic.Add("gxsyxw", "selected5/10207");
+            GaoPindic.Add("hbsyxw", "selected5/10208");
+            GaoPindic.Add("lnsyxw", "selected5/10209");
+            GaoPindic.Add("jssyxw", "selected5/10210");
+            GaoPindic.Add("zjsyxw", "selected5/10211");
             GaoPindic.Add("nmgsyxw", "selected5/10212");
-            //GaoPindic.Add("jsk3", "kuai3/10401");
-            //GaoPindic.Add("jlk3", "kuai3/10402");
-            //GaoPindic.Add("hebk3", "kuai3/10403");
-            //GaoPindic.Add("ahk3", "kuai3/10404");
+            GaoPindic.Add("jsk3", "kuai3/10401");
+            GaoPindic.Add("jlk3", "kuai3/10402");
+            GaoPindic.Add("hebk3", "kuai3/10403");
+            GaoPindic.Add("ahk3", "kuai3/10404");
             GaoPindic.Add("fjk3", "kuai3/10406");
-            //GaoPindic.Add("hbk3", "kuai3/10407");
-            ////GaoPindic.Add("bjk3", "kuai3/10408");北京快3期号不一致
-            //GaoPindic.Add("gxk3", "kuai3/10409");
-            //GaoPindic.Add("ssc", "sscs/10101");
-            //GaoPindic.Add("tjssc", "sscs/10103");
+            GaoPindic.Add("hbk3", "kuai3/10407");
+            //GaoPindic.Add("bjk3", "kuai3/10408");北京快3期号不一致
+            GaoPindic.Add("gxk3", "kuai3/10409");
+            GaoPindic.Add("ssc", "sscs/10101");
+            GaoPindic.Add("tjssc", "sscs/10103");
 
-            //QuanguoDic.Add("ssq", "10801");
-            //QuanguoDic.Add("sd", "10802");
-            //QuanguoDic.Add("qlc", "10803");
-            //QuanguoDic.Add("dlt", "10804");
-            //QuanguoDic.Add("pls", "10805");
-            //QuanguoDic.Add("plw", "10806");
+            QuanguoDic.Add("ssq", "10801");
+            QuanguoDic.Add("sd", "10802");
+            QuanguoDic.Add("qlc", "10803");
+            QuanguoDic.Add("dlt", "10804");
+            QuanguoDic.Add("pls", "10805");
+            QuanguoDic.Add("plw", "10806");
 
             //红球个数字典
             RedBallGameCode.Add("ssq",1);
