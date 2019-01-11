7872349fad647786917133b7bb6d3337b1d1adda
刘杭州 <348846313@qq.com>
2019-01-11 16:50:21 +0800

    代理请求

---
+++ Smart.Core/Lottery.GatherApp/Helper/UserAgengHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lottery.GatherApp.Helper
+{
+   public class UserAgengHelper
+    {
+        public DateTime Timer { get; set; }//记录上一次更换IP的时间
+
+    }
+}
